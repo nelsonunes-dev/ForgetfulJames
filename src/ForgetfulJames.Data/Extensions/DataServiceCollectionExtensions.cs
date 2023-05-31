a0863ae2a418fbe469062f1fa1e877f9d0763511
Nelson Nunes <nelson.nunes@eoh.com>
2023-05-31 20:10:52 +0200

    Implemented Data/Persistence Layer

---
+++ src/ForgetfulJames.Data/Extensions/DataServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ForgetfulJames.Data.Extensions
+{
+    public static class DataServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDataServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
