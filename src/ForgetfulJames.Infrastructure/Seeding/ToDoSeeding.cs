fdc13bae03fc0f4a3be8a8680a5cc3821f420b79
Nelson Nunes <nelson.nunes@eoh.com>
2023-06-01 10:22:30 +0200

    Added ToDo code-first configurations. Added Initial Migration Added ToDo Seeding functionality.

---
+++ src/ForgetfulJames.Infrastructure/Seeding/ToDoSeeding.cs
@@ -0,0 +1,12 @@
+﻿using ForgetfulJames.Domain.Entities;
+using ForgetfulJames.Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgetfulJames.Infrastructure.Seeding
+{
+    public class ToDoSeeding : SeedingBase<ToDo> { }
+}
