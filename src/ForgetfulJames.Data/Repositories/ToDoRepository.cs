a3b72c1974892a92f545df6f5afb5ed07e62b73f
Nelson Nunes <nelson.nunes@eoh.com>
2023-06-01 10:25:55 +0200

    Implemented ToDoRepository Updated dependencies required.

---
+++ src/ForgetfulJames.Data/Repositories/ToDoRepository.cs
@@ -0,0 +1,18 @@
+﻿using ForgetfulJames.Data.Abstractions.Repositories;
+using ForgetfulJames.Data.Common;
+using ForgetfulJames.Domain.Entities;
+using ForgetfulJames.Infrastructure;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgetfulJames.Data.Repositories
+{
+    public class ToDoRepository : Repository<ToDo>, IToDoRepository
+    {
+        public ToDoRepository(ApplicationDbContext dbContext, ILogger<ToDo> logger) : base(dbContext, logger) { }
+    }
+}
