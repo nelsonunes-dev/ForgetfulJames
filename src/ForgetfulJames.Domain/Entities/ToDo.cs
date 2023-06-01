d31b8febc53fa3ee1f20df0c90717bc2b6de5445
Nelson Nunes <nelson.nunes@eoh.com>
2023-06-01 10:28:05 +0200

    Updated Domain Layer

---
+++ src/ForgetfulJames.Domain/Entities/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using ForgetfulJames.Domain.Common;
+using ForgetfulJames.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgetfulJames.Domain.Entities
+{
+    public class ToDo : EntityBase<Guid>
+    {
+        public DateTime DueDate { get; set; }
+        public byte[] Image { get; set; } = Array.Empty<byte>();
+        public string Name { get; set; } = string.Empty;
+        public ToDoImportance Starred { get; set; } = ToDoImportance.Normal;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
